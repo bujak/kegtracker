40796ef2c41c1f595f785375b0c4e85e069a9d2a
Tomasz Bujakowski <bujakowski@gmail.com>
2018-03-04 00:22:59 +0100

    Add findAll in KegRepository

--- src/main/java/com/bujak/kegtracker/kegs/KegRepository.java
+++ src/main/java/com/bujak/kegtracker/kegs/KegRepository.java
@@ -3,6 +3,11 @@ package com.bujak.kegtracker.kegs;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KegRepository extends PagingAndSortingRepository<Keg, Long> {
+
+    List<Keg> findAll();
+
 }
