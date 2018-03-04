bac121f96e2bcb26e9bbfe39f097b85e2847ac50
Tomasz Bujakowski <bujakowski@gmail.com>
2018-03-04 15:28:09 +0100

    Change structure of Keg class

--- src/main/java/com/bujak/kegtracker/kegs/Keg.java
+++ src/main/java/com/bujak/kegtracker/kegs/Keg.java
@@ -1,47 +1,36 @@
 package com.bujak.kegtracker.kegs;
 
 import javax.persistence.*;
-import java.util.Arrays;
 import java.util.List;
 
 @Entity
 public class Keg {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private Card card1;
-    private Card card2;
-    private Card card3;
-    private Card card4;
-    private Card card5;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType. STRING)
+    private List<Card> cards;
 
     public Keg() {
     }
 
     public Keg(List<Card> cards) {
-        assignCards(cards);
-      }
+        this.cards = cards;
+    }
 
     public Integer getKegId() {
         return id.intValue();
     }
 
     public List<Card> getCards() {
-        return Arrays.asList(card1, card2, card3, card4, card5);
+        return this.cards;
     }
 
     public void setCards(List<Card> cards) {
-        assignCards(cards);
-    }
-
-    private void assignCards(List<Card> cards) {
-        this.card1 = cards.get(0);
-        this.card2 = cards.get(1);
-        this.card3 = cards.get(2);
-        this.card4 = cards.get(3);
-        this.card5 = cards.get(4);
+        this.cards = cards;
     }
 
     @Override
@@ -52,21 +41,13 @@ public class Keg {
         Keg keg = (Keg) o;
 
         if (id != null ? !id.equals(keg.id) : keg.id != null) return false;
-        if (card1 != keg.card1) return false;
-        if (card2 != keg.card2) return false;
-        if (card3 != keg.card3) return false;
-        if (card4 != keg.card4) return false;
-        return card5 == keg.card5;
+        return cards != null ? cards.equals(keg.cards) : keg.cards == null;
     }
 
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (card1 != null ? card1.hashCode() : 0);
-        result = 31 * result + (card2 != null ? card2.hashCode() : 0);
-        result = 31 * result + (card3 != null ? card3.hashCode() : 0);
-        result = 31 * result + (card4 != null ? card4.hashCode() : 0);
-        result = 31 * result + (card5 != null ? card5.hashCode() : 0);
+        result = 31 * result + (cards != null ? cards.hashCode() : 0);
         return result;
     }
 
@@ -74,11 +55,7 @@ public class Keg {
     public String toString() {
         return "Keg{" +
                 "id=" + id +
-                ", card1=" + card1 +
-                ", card2=" + card2 +
-                ", card3=" + card3 +
-                ", card4=" + card4 +
-                ", card5=" + card5 +
+                ", cards=" + cards +
                 '}';
     }
 }
